ee9b2f7a0c2d6de4d131e43a26fb36faf231abbf
springdom <manly13@hotmail.com>
2014-07-30 02:05:29 +0000

    First Commit

---
+++ splatter/config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Splatter::Application.config.secret_token = '0c226ce587ed740f80900419a38e4d2f24cb176c3dca6ed6e672a61399bd4f42cf2e40450e6043ffb49716aea8bc6ab7ae38e8bc3fed9df95ba9a68fcd07fa1b'
---
+++ weblog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Weblog::Application.config.secret_key_base = '73395173a8e55a7e3d8f8c255d8f5b8206a0e6aef86136dcbcc3353047659b2ec33a3f6ebc66134428a1dda902ecad05be62a64e0f441816c4a2c60a553482fe'
