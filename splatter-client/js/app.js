48097cd58571088793f15f5fbb60e02fbf371645
root <root@ip-172-31-30-147.us-west-1.compute.internal>
2014-09-11 10:03:03 +0000

    M2 Progresss update

--- splatter-client/js/app.js
+++ splatter-client/js/app.js
@@ -1,36 +1,41 @@
 (function() { // We use this anonymous function to create a closure.
     var app = angular.module('splatter-web', ['ngResource']);
     app.factory('User', function($resource) {
-        return $resource('http://taylor.sqrawler.com:3000/users/:id.json', {
+        return $resource('http://taylor.sqrawler.com/api/users/:id.json', {
             id: '@id'
         }, {
             update: {
-                method: 'PUT',
-                url: 'http://taylor.sqrawler.com/api/users/:id.json',
+                method: 'PUT'
+            },
+            'mySplatts': {
+                method: 'GET',
+                url: "http://taylor.sqrawler.com/api/users/splatts/:id.json",
                 isArray: true
             },
-            save: {
+            'myFeeds': {
+                method: 'GET',
+                url: "http://taylor.sqrawler.com/api/users/splatts-feed/:id.json",
+                isArray: true
+            },
+            'addSplatt': {
                 method: 'POST',
-                url: 'http://taylor.sqrawler.com:3000/users/',
-                isArray: true
+                url: "http://taylor.sqrawler.com/api/splatts.json"
             },
-            listSplatt: {
-                method: 'GET',
-                url: 'http://taylor.sqrawler.com/api/users/splatts-feed/:id',
-                isArray: true
+            'follow': {
+                method: 'POST',
+                url: 'http://taylor.sqrawler.com/api/users/follows/'
             },
-            splatts: {
-                method: 'GET',
-                url: 'http://taylor.sqrawler.com/api/users/splatts/:id',
-                isArray: true
-            },
+            'unfollow': {
+                method: 'DELETE',
+                url: 'http://taylor.sqrawler.com/api/users/follows/:id/:follows_id'
+            }
         });
     });
     app.factory('Splatt', function($resource) {
-        return $resource('http://taylor.sqrawler.com/api/splatts/:id.json', {}, {
+        return $resource('http://.sqrawler.com/api/splatts/:id.json', {}, {
             save: {
                 method: 'POST',
-                url: 'http://taylor.sqrawler.com/api/splatts'
+                url: 'http://.sqrawler.com/api/splatts'
             }
         })
     });
@@ -43,22 +48,23 @@
                 id: i
             });
         };
-
+        //user login and CRUD
         this.login = function() {
-
             this.loggedin_user = this.getUser(this.data.id);
-
+            this.splatts = User.mySplatts({
+                id: this.data.id
+            });
+            this.feeds = User.myFeeds({
+                id: this.data.id
+            });
             this.data = {};
         };
-
         this.updateUser = function() {
-
             // regexp from https://github.com/angular/angular.js/blob/master/src/ng/directive/input.js#L4
             this.loggedin_user.name = this.data.name;
             this.loggedin_user.email = this.data.email;
             this.loggedin_user.blurb = this.data.blurb;
             this.loggedin_user.$update();
-
             this.data = {};
         };
         this.createUser = function() {
@@ -67,15 +73,7 @@
             u.email = this.data.cemail;
             u.password = this.data.cpassword;
             u.blurb = this.data.cblurb;
-            u.$save({
-                user: {
-                    name: u.name,
-                    email: u.email,
-                    password: u.password,
-                    blurb: u.blurb
-                }
-            });
-            console.log(u);
+            u.$save();
             this.data = {};
         };
         this.delUser = function() {
@@ -84,48 +82,23 @@
                 id: this.data.delid
             });
         };
-    });
-    /* Splatts Controller*/
-    app.controller('SplattsController', function(Splatt) {
-        this.data = {};
-
-        this.SplattsFeed = function() {
-            this.data = {};
-            this.viewSplatt = function() {
-                splatts_feed = this.data.splatts_feed;
-                this.splatts_feed = User.listSplatt({
-                    id: this.data.id
-                });
-                this.data = {};
+        //Splatts
+        this.PostSplatt = function() {
+            postData = {
+                "splatt": {
+                    "body": this.data.splattbody,
+                    "user_id": this.loggedin_user.id
+                }
             }
+            User.addSplatt({}, postData);
+            this.data = {}
         };
-        this.ViewInformation = function() {
+        this.info = function() {
+            this.info_user = this.getUser(this.data.infoid);
+            this.splattsinfo = User.mySplatts({
+                id: this.data.infoid
+            });
             this.data = {};
-            this.viewInfo = function() {
-                var user_id = this.data.id;
-                this.u = User.get({
-                    id: user_id
-                });
-                this.splatts = User.splatts({
-                    id: user_id
-                });
-                this.data = {};
-            }
-        };
-        this.createSplatt = function() {
-            this.data = {};
-            this.composeSplatt = function() {
-                var body = this.data.body;
-                var id = this.data.id;
-                Splatt.save({
-                    splatt: {
-                        body: body,
-                        user_id: id
-                    }
-                });
-                this.data = {};
-            }
         };
     });
-
 })();
--- splatter/CScript.sh
+++ splatter/CScript.sh
@@ -1,31 +1,31 @@
 #1. Create 3 Users 
-curl -i -H "Content-type: application/json" -X POST http://taylor.sqrawler.com:3000/users -d '{"user": {"email":"Jon@splatter.com", "name":"John Doe", "password":"P@ssw0rd"}}'
-curl -i -H "Content-type: application/json" -X POST http://taylor.sqrawler.com:3000/users -d '{"user": {"email":"Matt@splatter.com", "name":"Matthew Taylorr", "password":"P@ssw0rd"}}'
-curl -i -H "Content-type: application/json" -X POST http://taylor.sqrawler.com:3000/users -d '{"user": {"email":"Jea@splatter.com", "name":"Jean William", "password":"P@ssw0rd"}}'
+curl -i -H "Content-type: application/json" -X POST http://taylor.sqrawler.com/api/users -d '{"user": {"email":"Jon@splatter.com", "name":"Two Pack", "password":"P@ssw0rd"}}'
+curl -i -H "Content-type: application/json" -X POST http://taylor.sqrawler.com/api/users -d '{"user": {"email":"Matt@splatter.com", "name":"Manny GEE", "password":"P@ssw0rd"}}'
+curl -i -H "Content-type: application/json" -X POST http://taylor.sqrawler.com/api/users -d '{"user": {"email":"Jea@splatter.com", "name":"ASD", "password":"P@ssw0rd"}}'
 
 #2. Create 5 Splatts for each user in a manner that mixes the chronological 
 #order of the splatts between 3 users
-curl -i -H "Content-type: application/json" -X POST http://taylor.sqrawler.com:3000/splatts -d '{"splatt": {"body":"Hi low", "user_id":3}}'
-curl -i -H "Content-type: application/json" -X POST http://taylor.sqrawler.com:3000/splatts -d '{"splatt": {"body":"Splatt  2", "user_id":1}}'
-curl -i -H "Content-type: application/json" -X POST http://taylor.sqrawler.com:3000/splatts -d '{"splatt": {"body":"3 Splatt", "user_id":2}}'
-curl -i -H "Content-type: application/json" -X POST http://taylor.sqrawler.com:3000/splatts -d '{"splatt": {"body":"Good Bye", "user_id":1}}'
-curl -i -H "Content-type: application/json" -X POST http://taylor.sqrawler.com:3000/splatts -d '{"splatt": {"body":"Splatt", "user_id":3}}'
+curl -i -H "Content-type: application/json" -X POST http://taylor.sqrawler.com/api/splatts -d '{"splatt": {"body":"Hi low", "user_id":3}}'
+curl -i -H "Content-type: application/json" -X POST http://taylor.sqrawler.com/api/splatts -d '{"splatt": {"body":"Splatt  2", "user_id":1}}'
+curl -i -H "Content-type: application/json" -X POST http://taylor.sqrawler.com/api/splatts -d '{"splatt": {"body":"3 Splatt", "user_id":2}}'
+curl -i -H "Content-type: application/json" -X POST http://taylor.sqrawler.com/api/splatts -d '{"splatt": {"body":"Good Bye", "user_id":1}}'
+curl -i -H "Content-type: application/json" -X POST http://taylor.sqrawler.com/api/splatts -d '{"splatt": {"body":"Splatt", "user_id":3}}'
 
 #3. Cause the first user to follow the other user:
-curl -i -H "Content-type: application/json" -X POST http://taylor.sqrawler.com:3000/users/follows -d '{"id":1, "follows_id":2}'
-curl -i -H "Content-type: application/json" -X POST http://taylor.sqrawler.com:3000/users/follows -d '{"id":1, "follows_id":3}'
+curl -i -H "Content-type: application/json" -X POST http://taylor.sqrawler.com/api/users/follows -d '{"id":1, "follows_id":2}'
+curl -i -H "Content-type: application/json" -X POST http://taylor.sqrawler.com/api/users/follows -d '{"id":1, "follows_id":3}'
 
 #4 Show User 1's Splatts:
-curl -i -H "Content-type: application/json" -X GET http://taylor.sqrawler.com:3000/users/splatts/1
+curl -i -H "Content-type: application/json" -X GET http://taylor.sqrawler.com/api/users/splatts/1
 
 #5. Show users who User 1 Follows:
-curl -i -H "Content-type: application/json" -X GET http://taylor.sqrawler.com:3000/users/follows/1
+curl -i -H "Content-type: application/json" -X GET http://taylor.sqrawler.com/api/users/follows/1
 
 #6. Get users news feed:
-curl -i -H "Content-type: application/json" -X GET http://taylor.sqrawler.com:3000/users/splatts-feed/1
+curl -i -H "Content-type: application/json" -X GET http://taylor.sqrawler.com/api/users/splatts-feed/1
 
 #7. causes the first user to unfollow the third user
-curl -i -H "Content-type: application/json" -X DELETE http://taylor.sqrawler.com:3000/users/follows/1/3
+curl -i -H "Content-type: application/json" -X DELETE http://taylor.sqrawler.com/api/users/follows/1/3
 
 #8.display the first users news feed after unfollowing the third
-curl -i -H "Content-type: application/json" -X GET http://taylor.sqrawler.com:3000/users/splatts-feed/1
+curl -i -H "Content-type: application/json" -X GET http://taylor.sqrawler.com/api/users/splatts-feed/1
