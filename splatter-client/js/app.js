4590385852ae0649e020e8140b018c7c37749e76
springdom <manly13@hotmail.com>
2014-09-11 02:46:41 +0000

    M2 still working on

--- splatter-client/js/app.js
+++ splatter-client/js/app.js
@@ -1,13 +1,41 @@
 (function() { // We use this anonymous function to create a closure.
     var app = angular.module('splatter-web', ['ngResource']);
     app.factory('User', function($resource) {
-        return $resource('http://taylor.sqrawler.com/api/users/:id.json', {
-            id: '@id'},
-        {'save': {method: 'POST', url: "http://taylor.sqrawler.com/api/users.json"},
-         'update': {method:'PUT'}
+        return $resource('http://taylor.sqrawler.com:3000/users/:id.json', {
+            id: '@id'
+        }, {
+            update: {
+                method: 'PUT',
+                url: 'http://taylor.sqrawler.com/api/users/:id.json',
+                isArray: true
+            },
+            save: {
+                method: 'POST',
+                url: 'http://taylor.sqrawler.com:3000/users/',
+                isArray: true
+            },
+            listSplatt: {
+                method: 'GET',
+                url: 'http://taylor.sqrawler.com/api/users/splatts-feed/:id',
+                isArray: true
+            },
+            splatts: {
+                method: 'GET',
+                url: 'http://taylor.sqrawler.com/api/users/splatts/:id',
+                isArray: true
+            },
         });
     });
-    app.controller('UserController', function(User, $scope) {
+    app.factory('Splatt', function($resource) {
+        return $resource('http://taylor.sqrawler.com/api/splatts/:id.json', {}, {
+            save: {
+                method: 'POST',
+                url: 'http://taylor.sqrawler.com/api/splatts'
+            }
+        })
+    });
+    var error = null;
+    app.controller('UserController', function(User) {
         this.data = {};
         this.ulist = User.query();
         this.getUser = function(i) {
@@ -15,15 +43,22 @@
                 id: i
             });
         };
+
         this.login = function() {
+
             this.loggedin_user = this.getUser(this.data.id);
+
             this.data = {};
         };
+
         this.updateUser = function() {
+
+            // regexp from https://github.com/angular/angular.js/blob/master/src/ng/directive/input.js#L4
             this.loggedin_user.name = this.data.name;
             this.loggedin_user.email = this.data.email;
             this.loggedin_user.blurb = this.data.blurb;
             this.loggedin_user.$update();
+
             this.data = {};
         };
         this.createUser = function() {
@@ -32,7 +67,15 @@
             u.email = this.data.cemail;
             u.password = this.data.cpassword;
             u.blurb = this.data.cblurb;
-            u.$save();
+            u.$save({
+                user: {
+                    name: u.name,
+                    email: u.email,
+                    password: u.password,
+                    blurb: u.blurb
+                }
+            });
+            console.log(u);
             this.data = {};
         };
         this.delUser = function() {
@@ -40,11 +83,49 @@
             User.delete({
                 id: this.data.delid
             });
-            //get your user from where ever.
-            var user = getSomeUser();
-
-            // set your user permissions
-            // here's some contrived example.
         };
     });
+    /* Splatts Controller*/
+    app.controller('SplattsController', function(Splatt) {
+        this.data = {};
+
+        this.SplattsFeed = function() {
+            this.data = {};
+            this.viewSplatt = function() {
+                splatts_feed = this.data.splatts_feed;
+                this.splatts_feed = User.listSplatt({
+                    id: this.data.id
+                });
+                this.data = {};
+            }
+        };
+        this.ViewInformation = function() {
+            this.data = {};
+            this.viewInfo = function() {
+                var user_id = this.data.id;
+                this.u = User.get({
+                    id: user_id
+                });
+                this.splatts = User.splatts({
+                    id: user_id
+                });
+                this.data = {};
+            }
+        };
+        this.createSplatt = function() {
+            this.data = {};
+            this.composeSplatt = function() {
+                var body = this.data.body;
+                var id = this.data.id;
+                Splatt.save({
+                    splatt: {
+                        body: body,
+                        user_id: id
+                    }
+                });
+                this.data = {};
+            }
+        };
+    });
+
 })();
--- splatter/app/controllers/users_controller.rb
+++ splatter/app/controllers/users_controller.rb
@@ -1,100 +1,102 @@
-  
 class UsersController < ApplicationController
-  before_filter :set_headers
-  # GET /users
-  # GET /users.json
-  def index
-    @users = User.all
-
-    render json: @users
-  end
-
-  # GET /users/1
-  # GET /users/1.json
-  def show
-    @user = User.find(params[:id])
-
-    render json: @user
-  end
-
-  # POST /users
-  # POST /users.json
-  def create
-    @user = User.new(user_params(params[:user]))
-
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
-  def update
-    @user = User.find(params[:id])
-
-    if @user.update(user_params(params[:user]))
-      head :no_content
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /users/1
-  # DELETE /users/1.json
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-
-    head :no_content
-  end
-  def splatts
-	@user = User.find(params[:id])
-	render json: @user.splatts
-  end
-  # GET /users/splatts-feed/1
-  def splatts_feed
-	@feed = Splatt.find_by_sql("SELECT * FROM follows JOIN splatts ON follows.followed_id = splatts.user_id WHERE follows.follower_id = #{params[:id]} ORDER BY created_at DESC")
-	render json: @feed
-  end
-  
-  def show_follows
-	@user = User.find(params[:id])
-	render json: @user.follows
-  end
-
-
-  def show_followers
-	@user = User.find(params[:id])	
-    render json: @user.followed_by
-  end
-
-  def add_follows
-	@user1 = User.find(params[:id])
-	@user2 = User.find(params[:follows_id])
-  if @user1.follows << @user2
-	render json: @user1.follows
-  else
-	render json: @user1.error, status: :unprocessable_entity
-  end
-  end
-
-  def delete_follows
-	@user1 = User.find(params[:id])
-	@user2 = User.find(params[:follows_id])
-    if @user1.follows.delete(@user2)
-		render json: @user1.follows
-    else
-		render json: @user1.error, status: :unprocessable_entity
-  end	
-
- end
- private
-  def user_params(params)
-	params.permit(:email, :password, :name, :blurb)
-  end	
-   	def set_headers
-		headers['Access-Control-Allow-Origin'] = '*'
-	end
+before_filter :cors_preflight_check
+after_filter :cors_set_access_control_headers
+# GET /users
+# GET /users.json
+def index
+@users = User.all
+render json: @users
+end
+# GET /users/splatts-feed/1
+def splatts_feed
+@feed = Splatt.find_by_sql("SELECT splatts.body, splatts.user_id, splatts.id, splatts.created_at FROM splatts JOIN follows ON follows.followed_id=splatts.user_id WHERE follows.follower_id=#{params[:id]} ORDER BY created_at DESC")
+render json: @feed
+end
+# GET /users/splatts/[:id]
+def splatts
+@user = User.find(params[:id])
+render json: @user.splatts
+end
+# GET /users/follows/[:id]
+def show_follows
+@user = User.find(params[:id])
+render json: @user.follows
+end
+# POST /users/follows
+def add_follows
+@user = User.find(params[:id])
+@follows = User.find(params[:follows_id])
+if @user.follows << @follows
+head :no_content
+else
+render json: @user.errors, status: :unprocessable_entity
+end
+end
+# DELETE /users/follows/1/2
+def delete_follows
+@user = User.find(params[:id])
+@follows = User.find(params[:follows_id])
+if @user.follows.delete(@follows)
+head :no_content
+else
+render json: @user.errors, status: :unprocessable_entity
+end
+end
+# GET /users/1
+# GET /users/1.json
+def show
+@user = User.find(params[:id])
+render json: @user
+end
+# POST /users
+# POST /users.json
+def create
+@user = User.new(user_params(params))
+if @user.save
+render json: @user, status: :created, location: @user
+else
+render json: @user.errors, status: :unprocessable_entity
+end
+end
+# PATCH/PUT /users/1
+# PATCH/PUT /users/1.json
+def update
+@user = User.find(params[:id])
+if @user.update(user_params(params))
+head :no_content
+else
+render json: @user.errors, status: :unprocessable_entity
+end
+end
+# DELETE /users/1
+# DELETE /users/1.json
+def destroy
+@user = User.find(params[:id])
+@user.destroy
+head :no_content
+end
+private
+def user_params(params)
+logger.info "params: #{params}"
+params.permit(:id, :name, :email, :password, :blurb)
+end
+def set_headers
+headers['Access-Control-Allow-Origin'] = '*'
+end
+# For all responses in this controller, return the CORS access control headers.
+def cors_set_access_control_headers
+headers['Access-Control-Allow-Origin'] = '*'
+headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+headers['Access-Control-Max-Age'] = '1728000'
+end
+# # If this is a preflight OPTIONS request, then short-circuit the
+# # request, return only the necessary headers and return an empty
+# # text/plain.
+#
+def cors_preflight_check
+headers['Access-Control-Allow-Origin'] = '*'
+headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
+headers['Access-Control-Max-Age'] = '1728000'
+end
 end
